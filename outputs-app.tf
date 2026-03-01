5aa1ea4375722b079f0ead174441f0461a05ec94
Siva Suribabu <108812647+SivaSuribabu@users.noreply.github.com>
2026-03-01 10:25:06 +0530

    Delete outputs-app.tf

--- outputs-app.tf
+++
@@ -1,7 +0,0 @@
-output "application_name" {
-  value = aws_elastic_beanstalk_application.app.name
-}
-
-output "version_label" {
-  value = aws_elastic_beanstalk_application_version.app_version.name
-}
