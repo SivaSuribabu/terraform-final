ed097279cf6a839c79bcb368f3b4104890f66a2b
Siva Suribabu <108812647+SivaSuribabu@users.noreply.github.com>
2026-03-01 10:23:59 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-21-02-2026"
-    key            = "beanstalk/terraform.tfstate"
-    region         = "ap-south-1"
-    use_lockfile = true
-    encrypt        = true
-  }
-}
